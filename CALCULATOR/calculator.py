8e5697a75adc2544f96646b9c34d59d6b262061b
Martasantenbo <martasan.donuts@gmail.com>
2022-08-22 15:43:29 +0200

    add(CALCULATOR) added a very simple calculator

---
+++ CALCULATOR/calculator.py
@@ -0,0 +1,7 @@
+# Simple Calculator for two Numbers
+
+numOne, numTwo = (float, input("Input two numbers separated by a comma: ").split(","))
+print(numOne + numTwo)
+print(numOne - numTwo)
+print(numOne * numTwo)
+print(numOne / numTwo)
